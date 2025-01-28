fb4d4b954a56b1e6391b52561220c5bd062e665a
Kostiantyn <kostya20022210@gmail.com>
2025-01-28 21:02:10 +0200

    Update palindrome check function to ignore spaces

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@
  */
 function isPalindrome(str) {
     if (typeof str !== 'string') return false;
-    return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+    let strWithoutSpace = str.toLowerCase().split(' ').join('');
+    return strWithoutSpace === strWithoutSpace.split('').reverse().join('');
 }
 
 console.log(isPalindrome('RaceCar')); // true
 console.log(isPalindrome('Able was I ere I saw Elba')); // true
+console.log(isPalindrome('A man a plan a canal Panama')); // true
 console.log(isPalindrome('Hello World!')); // false
 console.log(isPalindrome(150051)); // false, must be a string
